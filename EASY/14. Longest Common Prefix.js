a855c3e62856e7b3fd165b6fcee1f8185c7c7247
not-thanhtai <tai606760@gmail.com>
2024-02-02 14:44:32 +0700

    SOLVED: 14. Longest Common Prefix

---
+++ EASY/14. Longest Common Prefix.js
@@ -0,0 +1,17 @@
+"use strict";
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+  let res = "";
+  for (let i = 0; i < strs[0].length; i++) {
+    for (const s of strs) {
+      if (i === s.length || s[i] !== strs[0][i]) {
+        return res;
+      }
+    }
+    res += strs[0][i];
+  }
+  return res;
+};
